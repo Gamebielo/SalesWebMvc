badc72af67993009be2ee8ba8167fa0f5abc4fb5
Gabriel <gabrielcarvalho20@gmail.com>
2021-09-22 09:43:44 -0300

    Fields Validation
    - Validação primeiro no JavaScript, caso o JS esteja desabilitado no browser, validação feita pelo servidor

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,16 +8,28 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")] // Obrigatório
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")] // Obrigatório
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)] // Transformando a string em um link mail
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")] // Obrigatório
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)] // Data sem horas e minutos
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")] // Obrigatório
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")] // Duas casas decimais
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
