3bdce34fb54fcbfc82e23f85c8baca77cfdc5d06
Gabriel <gabrielcarvalho20@gmail.com>
2021-10-20 21:03:44 -0300

    Code Updated

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -4,6 +4,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
+    /// <summary>
+    /// Classe responsável pelas propriedades dos registros de vendas.
+    /// </summary>
     public class SalesRecord
     {
         public int Id { get; set; }
@@ -11,14 +14,14 @@ namespace SalesWebMvc.Models
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:F2}")] // Duas casas decimais
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
 
         public SaleStatus Status { get; set; }
 
         public Seller Seller { get; set; }
 
-        public SalesRecord() { }
+        public SalesRecord() {}
 
         public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -1,12 +1,16 @@
-﻿using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
+    /// <summary>
+    /// Classe responsável pela regra de negócio referente aos registros de navegação das vendas.
+    /// </summary>
     public class SalesRecordService
     {
         private readonly SalesWebMvcContext _context;
@@ -16,21 +20,50 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
+        /// <summary>
+        /// Método responsável pela pesquisa de registro de venda por data.
+        /// </summary>
+        /// <param name="minDate">Define uma data mínima para a pesquisa.</param>
+        /// <param name="maxDate">Define uma data maxima para a pesquisa.</param>
+        /// <returns>Registro da uma venda de acordo com a data especificada.</returns>
         public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
-            // Constrói um obj IQueryble com base no DbSet que é o _context.SalesRecord
             var result = from obj in _context.SalesRecord select obj;
-            if (minDate.HasValue)
-            {
-                result = result.Where(x => x.Date >= minDate.Value);
+            if (minDate.HasValue) {
+                result = result.Where(x => x.Date >= minDate);
             }
             if (maxDate.HasValue) {
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
+
             return await result
-                .Include(x => x.Seller) // Join das tabelas
-                .Include(x => x.Seller.Department) // Join das tabelas
-                .OrderByDescending(x => x.Date) 
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Método responsável pela pesquisa de grupo de registro de vendas por data.
+        /// </summary>
+        /// <param name="minDate">Define uma data mínima para a pesquisa.</param>
+        /// <param name="maxDate">Define uma data maxima para a pesquisa.</param>
+        /// <returns>Registro da um grupo de vendas de acordo com a data especificada.</returns>
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue) {
+                result = result.Where(x => x.Date >= minDate);
+            }
+            if (maxDate.HasValue) {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
                 .ToListAsync();
         }
     }
